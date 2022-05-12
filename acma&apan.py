b240cfce29ca1f2fa93061b29c03263782ee9633
SehribanSevin <38330217+SehribanSevin@users.noreply.github.com>
2022-05-12 13:33:12 +0300

    It's about usage variables

---
+++ acma&apan.py
@@ -0,0 +1,16 @@
+#Simple Mad Libs 
+#We are entering three input and we'll create a short poem
+
+Soru1=input("Asur kralı 2.Asurbanipal'in oğlu:")
+Soru2=input("Kadın kıyafetleri giyen Ninova'nın kuşatılması sırasında büyük bir ateş yaktırıp cariye ve hizmetçileriyle intihar eden eski asur kralı:")
+Soru3=input("Homeros'un hocası:")
+Soru4=input("26. Mısır Hanedanlığı'nı yöneten kral:")
+
+
+print("Talihsizliklerin en kötüsü şöhretlisi bile er ya da geç felsefenin bitip tükenmek bilmeyen cesaretine teslim olur.Tıpkı en dirençli şehrin bile düşmanın hilekârlığına teslim olması gibi." + Soru1)
+print(",kutsal kitaplarda belirtildiği üzere Samaria'nın önünde üç yıl bekledi ama sonunda şehir düştü."+Soru2)
+print("Ninova'da düşmana karşı yedi sene dayanabildi.Truva on sene boyunca direnebildi ve"+Soru3)
+print("şerefi üstüne yemin edip söylediği üzere Azoth kapıları " +Soru4)
+print("'a ancak yirmi sene süren bir savunmanın ardından açıldı...")
+
+
